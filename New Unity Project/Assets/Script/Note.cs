130b972fc4b30a4325a07cee2bc6ade395f5366e
NGAEVA <ywt1230@163.com>
2017-04-18 09:09:30 -0700

    Project setting

---
+++ New Unity Project/Assets/Script/Note.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+    void Start()
+    {
+        rb.velocity = new Vector2(0, -speed);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
