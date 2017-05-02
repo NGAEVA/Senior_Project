ea1c90986be5bb9d2b4f1015ef75053b1402edc8
NGAEVA <ywt1230@163.com>
2017-05-01 20:30:43 -0700

    Activator changes

--- New Unity Project/Assets/Script/Activator.cs
+++ New Unity Project/Assets/Script/Activator.cs
@@ -6,8 +6,10 @@ public class Activator : MonoBehaviour {
     SpriteRenderer sr;
     public KeyCode key;
     bool active = false;
-    GameObject note;
+    GameObject note,GM;
     Color old;
+    public bool createNode;
+    public GameObject n;
 
     void Awake()
     {
@@ -16,21 +18,40 @@ public class Activator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         PlayerPrefs.SetInt("Score", 0);
+        GM = GameObject.Find("GameManager");
         old = sr.color;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown(key))
+
+        if(createNode)
         {
-            StartCoroutine(Pressed());
+            if(Input.GetKeyDown(key))
+            {
+                Instantiate(n, transform.position, Quaternion.identity);
+            }
         }
 
-	    if(Input.GetKeyDown(key) && active)
+        else
         {
-            Destroy(note);
-            AddScore();
-            active = false;
+            if (Input.GetKeyDown(key))
+            {
+                StartCoroutine(Pressed());
+            }
+
+            if (Input.GetKeyDown(key) && active)
+            {
+                Destroy(note);
+                GM.GetComponent<GameManager>().AddStreak();
+                AddScore();
+                active = false;
+            }
+
+            if (Input.GetKeyDown(key) && !active)
+            {
+                GM.GetComponent<GameManager>().ResetStreak();
+            }
         }
 	}
 
@@ -46,11 +67,12 @@ public class Activator : MonoBehaviour {
     void OnTriggerExit2D(Collider2D col)
     {
         active = false;
+        GM.GetComponent<GameManager>().ResetStreak();
     }
 
     void AddScore()
     {
-        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + 100);
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + GM.GetComponent<GameManager>().GetScore());
     }
 
 
